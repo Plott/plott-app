43fc211c18f58e87c79bd1def1718e0048c80310
Corey White <smortopahri@gmail.com>
2015-09-20 21:55:12 -0400

    Added map route

---
+++ client/app/map/map.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('plottAppApp')
+  .controller('MapCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/map/map.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('plottAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('map', {
+        url: '/map',
+        templateUrl: 'app/map/map.html',
+        controller: 'MapCtrl'
+      });
+  });
