6711d70f15208c922ed80f3d6abdb949417ac946
Corey White <smortopahri@gmail.com>
2015-10-03 22:01:59 -0400

    Added icon

--- client/app/user/buildings.controller.js
+++ client/app/user/buildings.controller.js
@@ -14,6 +14,7 @@
     $scope.buildingMarker;
     $scope.reset = reset;
     $scope.addBuilding = building.addBuilding;
+    $scope.zoomToBuilding = zoomToBuilding;
     self.reverseGeocoder = geocoder.reverse;
 
     L.mapbox.accessToken = MAPBOX.TOKEN;
@@ -27,7 +28,12 @@
       if(!$scope.buildingMarker){
         $scope.buildingMarker = L.marker(e.latlng, {
           draggable: true,
-          riseOnHover: true
+          riseOnHover: true,
+          icon: L.mapbox.marker.icon({
+              'marker-size': 'large',
+              'marker-symbol': 'building',
+              'marker-color': '#4393B9'
+          })
         }).addTo(map);
       }
       else {
@@ -35,27 +41,34 @@
       }
       $scope.building = $scope.buildingMarker.toGeoJSON();
       self.reverseGeocoder(e.latlng.lng, e.latlng.lat)
-        .then(function(res){
-          $log.log(res);
-          var data = res.data.features[0];
-          $scope.building.properties.address = data.address + ' ' + data.text;
-          $scope.building.properties.city = data.context[0].text;
-          $scope.building.properties.zip = parseInt(data.context[1].text, 10);
-          $scope.building.properties.state = data.context[2].text;
-        })
+        .then(setFormGeocode)
         .catch(function(err){
           $log.error(err);
         });
       $scope.$apply();
     });
 
-
-
-
     building.getUserBuildings(self.user)
       .then(function(buildings){
+        var fullAddress;
         $scope.buildings = buildings.data;
+        $scope.existingBuildings = L.geoJson($scope.buildings, {
+          pointToLayer: L.mapbox.marker.style,
+          style: function(feature) {
+            return {
+              // 'marker-size': 'large',
+              // 'marker-symbol': 'building',
+              'marker-color': '#fa0'
+            };
+          },
+          onEachFeature: function(feature, layer) {
+            fullAddress = '<h3><strong>' + feature.properties.address + '</strong><br>' + feature.properties.city + ', ' + feature.properties.state + ' ' + feature.properties.zip + '</h3>'
+            layer.bindPopup(fullAddress);
+          }
+        }).addTo(map);
+        console.log($scope.existingBuildings)
         socket.syncUpdates('building', $scope.buildings, function(event, item){
+          $scope.existingBuildings.addData(item);
           $scope.buildings.append(item);
         });
       })
@@ -66,6 +79,23 @@
     function reset() {
       $scope.building = {};
     }
+
+    function zoomToBuilding(geojson) {
+      L.geoJson(geojson, {
+        coordsToLatLng: function(coords) {
+          map.setView([coords[1], coords[0]], 18);
+        }
+      });
+    }
+
+    function setFormGeocode(res) {
+      var data = res.data.features[0];
+      $scope.building.properties.address = data.address + ' ' + data.text;
+      $scope.building.properties.city = data.context[0].text;
+      $scope.building.properties.zip = parseInt(data.context[1].text, 10);
+      $scope.building.properties.state = data.context[2].text;
+    }
+
   }
 
 })();
